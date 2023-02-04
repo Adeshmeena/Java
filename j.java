9f9f25aa8eb425b846e991f7b550dd12c167317a
adeshmeena <adeshmeena08@gmail.com>
2023-02-04 14:58:44 +0530

    first program

---
+++ j.java
@@ -0,0 +1,17 @@
+class jazzy{
+public static void main (String [] args){
+
+void show(){
+System.out.println("annu lolu");
+
+}
+}
+class B extend jazzy{
+void show(){
+System.out.println(" adesh a wealthy human");
+b t=new b;
+t.show;
+}
+
+}
+
